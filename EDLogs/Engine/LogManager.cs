a81e68acaec791294af0ec059ae9a1ab18eebff8
Jean-Baptiste Vovau <jb.vovau@gmail.com>
2018-10-10 18:17:47 +0200

    first commit

---
+++ EDLogs/Engine/LogManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EDLogs.Engine
+{
+    public class LogManager
+    {
+        public string Directory { get; set; }
+
+        public void ListenDirectory(string path)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = path;
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Filter = "*.log";
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            //Copies file to another directory.
+            Program.Log("File : " + e.ChangeType +" - " + e.FullPath);
+        }
+
+    }
+}
---
+++ EDLogs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using EDLogs.Engine;
+
+namespace EDLogs
+{
+    /// <summary>
+    /// Log parser for Elite Dangerous
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start EDLOGS !!");
+
+            // define new log engine
+            var engine = new LogManager();
+
+            engine.ListenDirectory(@"C:\samplelogs\");
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(5000);
+            }
+
+          
+        }
+
+        public static void Log(string message)
+        {
+            var line = string.Format("[{0}] {1}", DateTime.Now, message);
+            Console.WriteLine((line));
+        }
+    }
+}
